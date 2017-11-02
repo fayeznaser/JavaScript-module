10031c212ab61d7d8e02b22470a253fefda2ac46
Fayez Naser <32542479+fayeznaser@users.noreply.github.com>
2017-11-03 00:32:53 +0100

    Add files via upload

---
+++ week1/js/First assignment.js
@@ -0,0 +1,89 @@
+1
+var English = "Hello World";
+console.log(English);
+var Arabic = "Marhaba Aalam";
+console.log(Arabic);
+var Dutch = "Hallo Wereld";
+console.log(Dutch);
+var French = "bonjour le monde";
+console.log(French);
+var Fillipino = "Kumusta sa mundo";
+console.log(Fillipino);
+
+2
+console.log('I\'m awesome');
+
+3
+var x =7;
+console.log(x);
+console.log('the value of x will be : 7');
+
+4
+let y = "hello";
+console.log('the value of my string will be : hello');
+console.log(y);
+y= "hey";
+console.log('the value of my string will be : hey');
+
+5
+let z = 7.25;
+console.log(z);
+let a = Math.round(z);
+let h 
+if (z>a){
+    h=z
+    console.log(h);
+}
+else {
+    h=a;
+    console.log(h);
+}
+
+6
+let pet = []
+console.log('the value of my array is []');
+console.log(pet);
+let favoriteAnimal = ["cat","fish","dog"];
+console.log(favoriteAnimal);
+favoriteAnimal.push("baby pig");
+console.log(favoriteAnimal);
+
+7
+let myString = "this,is,a,test";
+console.log(myString);
+var theLenght= myString.length;
+console.log(theLenght);
+
+8
+let numberOne= 10;
+let numberTwo= "red";
+let numberThree= ["apple", "cherry"];
+let numberFour= true;
+console.log('the value of my variable numberOne is:' + numberOne);
+console.log('the value of my variable numberTwo is:' + numberTwo);
+console.log('the value of my variable numberThree is:' + numberThree);
+console.log('the value of my variable numberFour is:' + numberFour);
+typeof (numberOne);
+typeof (numberTwo);
+typeof (numberThree);
+typeof (numberFour);
+if (typeof(numberOne) == (numberTwo)){
+    console.log('SAME TYPE');
+}
+else{
+    console.log('NOT SAME TYPE');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
